539de97b4b8c5bfc77d21edc9052207707c51ed2
Xholile <xholilesamuel@gmail.com>
2026-05-30 14:02:53 +0200

    feat(frontend): setup file explorer feature structure

---
+++ filesystem-client/src/app/core/models/file-entry.model.ts
@@ -0,0 +1,9 @@
+export interface FileEntry {
+  name: string;
+  fullPath: string;
+  size: number;
+  extension: string | null;
+  isDirectory: boolean;
+  createdAt: Date;
+  permissions: string;
+}
---
+++ filesystem-client/src/app/features/file-explorer/pages/file-explorer/file-explorer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-explorer',
+  imports: [],
+  templateUrl: './file-explorer.html',
+  styleUrl: './file-explorer.scss',
+})
+export class FileExplorer {}
